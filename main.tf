f10f76b7de478447f80e828f7fce0b3d77a32a95
alhaj1986 <105738799+alhaj1986@users.noreply.github.com>
2024-08-07 17:11:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,54 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.region
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "BCTDemoVPC"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidr
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "BCTDemoPrivateSubnet"
+  }
+}
+
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+  
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "BCTDemoInstanceSG"
+  }
+}
 
 resource "aws_instance" "demo" {
-  ami       	 = "ami-0ec0e125bb6c6e8ec"
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.private.id
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
   tags = {
-	Name = "BCTDemoEC2Instance"
+    Name = var.instance_name
   }
 }
